81b789adda98f54fffd2557e3fb3e3a4ac77d3c9
Kornajun Roy <yarrrjun@gmail.com>
2019-10-07 11:04:38 -0400

    added queryURL and API request key

--- javascript/app.js
+++ javascript/app.js
@@ -1,10 +1,8 @@
-
-var queryURL = "";
-
+var searchTerm;
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key=Hig5M7FblcRTt7RMpTao9B4NGYPoxXyV"
 
 $.ajax({
-url:queryURL,
-method:"GET"
-}).then(function(response){
-    console.log(response);
+    url: queryURL,
+    method: "GET"
 })
+
