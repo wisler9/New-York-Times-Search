0d0ac09454f906ec02a15ff4e4d6b9f77a2b9cc9
Alex Puello <apuello88@yahoo.com>
2019-10-07 11:37:54 -0400

    test

--- javascript/app.js
+++ javascript/app.js
@@ -1,13 +1,21 @@
-var searchTerm;
+var searchTerm="glocations:('NEW YORK CITY')";
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key=Hig5M7FblcRTt7RMpTao9B4NGYPoxXyV";
 var jsonFile = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=&api-key=Hig5M7FblcRTt7RMpTao9B4NGYPoxXyV"
+var searchTerm;
 console.log("Javascript works");
 
 
 $.ajax({
-    url: jsonFile,
+    url: queryURL,
     method: "GET"
 }).then(function(response){
 console.log(response)
 })
+
+searchTerm = ("<div>")
+searchTerm.attr("")
+
+$("#search-term").
+
+
 
