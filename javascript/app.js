5f8dc344dafad97623a889a375ce2d653cff34b9
Alex Puello <apuello88@yahoo.com>
2019-10-07 11:17:05 -0400

    made the test

--- javascript/app.js
+++ javascript/app.js
@@ -1,11 +1,13 @@
 var searchTerm;
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchTerm+"&api-key=Hig5M7FblcRTt7RMpTao9B4NGYPoxXyV";
-
+var jsonFile = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=&api-key=Hig5M7FblcRTt7RMpTao9B4NGYPoxXyV"
 console.log("Javascript works");
 
 
 $.ajax({
-    url: queryURL,
+    url: jsonFile,
     method: "GET"
-});
+}).then(function(response){
+console.log(response)
+})
 
